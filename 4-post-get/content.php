47172eb61d70992b7546641adc37144443889191
Ksenia Martynova <hurstcain@gmail.com>
2020-09-13 19:10:24 +0300

    Добавлено задание из папки 3

---
+++ 4-post-get/content.php
@@ -0,0 +1,3 @@
+<?php
+// Выводим на экран сообщение, содержащее значение переменных first_name, last_name и age.
+echo 'Привет, меня зовут ' . $_POST["first_name"] . ' ' . $_POST["last_name"] . '. Мой возраст - ' . $_POST["age"];
